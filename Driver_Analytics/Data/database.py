ae628ef794c3916720e0cd2486912be28fe3293e
Marcus Fernandes <mvdafernandes@id.uff.br>
2026-02-13 13:38:24 -0300

    Delete Driver_Analytics directory

--- Driver_Analytics/Data/database.py
+++
@@ -1,9 +0,0 @@
-"""Legacy compatibility wrapper for database initialization."""
-
-from core.database import init_sqlite_schema
-
-
-def criar_banco() -> None:
-    """Backward compatible alias."""
-
-    init_sqlite_schema()
