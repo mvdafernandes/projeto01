c6da69a77d6f85dc30904c42f6d3059f93896fd9
Marcus Fernandes <mvdafernandes@id.uff.br>
2026-02-14 12:33:38 -0300

    Delete Driver_Analytics directory

--- Driver_Analytics/Data/supabase_client.py
+++
@@ -1,9 +0,0 @@
-"""Legacy compatibility wrapper for Supabase client."""
-
-from core.database import get_supabase_client
-
-
-def get_supabase():
-    """Backward compatible alias."""
-
-    return get_supabase_client()
