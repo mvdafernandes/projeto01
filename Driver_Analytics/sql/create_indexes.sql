00ef80a9bff922e7671fd64d35d49f41a0dd35f9
Marcus Fernandes <mvdafernandes@id.uff.br>
2026-03-05 21:12:50 -0300

    Delete Driver_Analytics directory

--- Driver_Analytics/sql/create_indexes.sql
+++
@@ -1,3 +0,0 @@
-create index if not exists idx_receitas_data on receitas(data);
-create index if not exists idx_despesas_data on despesas(data);
-create index if not exists idx_investimentos_data on investimentos(data);
